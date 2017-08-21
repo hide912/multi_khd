9ee9979f3d3253be33c9f8fee4fed11eae3d7887
M100112\student <hide912@hanmail.net>
2017-08-21 16:45:41 +0900

    두번째 커밋입니다.

--- src/lab2/Test2.java
+++ src/lab2/Test2.java
@@ -3,7 +3,8 @@ package lab2;
 public class Test2 {
 
 	public static void main(String[] args) {
-		System.out.println("Test1 class");
+		System.out.println("Test2 class");
+		System.out.println("Test2 class");
 
 	}
 
